88a1cdcd30c8eba1d327f081803c2a0f48979c9b
mdjuealhossain <mdjueal920977>
2023-09-21 02:30:28 +0600

    feat: layout

--- src/layouts/PortfulioLayout/index.tsx
+++ src/layouts/PortfulioLayout/index.tsx
@@ -2,18 +2,18 @@ import React, { FC } from "react";
 
 import { PortfulioLayoutProps } from "./Types";
 import { Box, Grid } from "@mui/material";
+import Sidebar from "src/widgets/SideBar";
 
 const PortfulioLayout: FC<PortfulioLayoutProps> = ({ children }) => {
   return (
     <>
       <Grid container>
-        <Grid item>Sidebar</Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>{children}</Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>Footer</Grid>
+        <Grid item width={310}>
+          <Sidebar />
+        </Grid>
+        <Grid item width="calc(100% - 310px)" bgcolor="red">
+          {children}
+        </Grid>
       </Grid>
     </>
   );
---
+++ src/widgets/SideBar/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { SidebarProps } from "./Types";
+
+const Sidebar: FC<SidebarProps> = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
